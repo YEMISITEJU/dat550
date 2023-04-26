f4848148569b80466a2ec9cab13a33b93bd18b2b
Yuriy Y <30913292+yurchenkoy@users.noreply.github.com>
2023-04-26 11:36:57 +0200

    Update pre_processing_yy.py

--- pre_processing_yy.py
+++ pre_processing_yy.py
@@ -1,8 +1,38 @@
 import cv2
 import numpy as np
 import json
+import matplotlib.pyplot as plt
+import os
+import time
 
-# simple face detection using cv2 (can be changed to pre-trained YOLO for better/faster results)
+data_dir = '/Volumes/storage/dfdc_preview_set'
+processed_data_dir = os.path.join(data_dir, "processed_data")
+os.makedirs(processed_data_dir, exist_ok=True)
+metadata_path = os.path.join(data_dir, 'dataset.json')
+
+# Load the metadata
+metadata = json.load(open(metadata_path))
+
+# filter out videos not in the "train" split
+train_videos = [filename for filename, info in metadata.items() if info["set"] == "train"]
+
+# Define augmentation parameters
+rotation_angles = [-10, -5, 0, 5, 10]  # list of rotation angles to randomly choose from
+horizontal_flips = [True, False]  # list of boolean values to randomly choose from
+
+# Define face detection parameters
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+scale_factor = 2  # factor to scale up the bounding box of the face
+
+extraction_rate = 5  # desired frame for extraction
+
+total_processing_time = 0
+num_processed_videos = 0
+
+# Count the number of mp4 files in the train folder
+num_train_videos = len(train_videos)
+print("Number of training videos: ", num_train_videos)
+
 def face_detection(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
@@ -17,8 +47,7 @@ def face_detection(frame):
         x = max(0, x)
         y = max(0, y)
         return frame[y:y + h * scale_factor, x:x + w * scale_factor]
-
-# simple data augmentation: horizontal flip and rotation    
+    
 def augment_frames(frame):
     angle = np.random.choice(rotation_angles)
     flip = np.random.choice(horizontal_flips)
@@ -28,40 +57,22 @@ def augment_frames(frame):
         frame = cv2.flip(frame, 1)
     return frame
 
-# resize frames to desired dimensions
 def resized_frames(frame, size=256):
     return cv2.resize(frame, (size, size))
 
-# normalize pixel values to range [0, 1]
 def normalize_frames(frames):
     mean = np.mean(frames, axis=(0,1,2)) / 255
     std = np.std(frames, axis=(0,1,2)) / 255
     frame = (frames / 255 - mean) / std
     return frame
 
-
-
-# Path to the dataset
-data_dir = '/Users/yuriyyurchenko/Documents/UiS/Semester_4/DataMining/Project/dfdc_train_part_10/'
-metadata_path = data_dir + 'metadata.json'
-metadata = json.load(open(metadata_path)) # load metadata
-
-# filter out videos not in the "train" split
-train_videos = [filename for filename, info in metadata.items() if info["split"] == "train"]
-train_videos = train_videos[:5] # using 5 for testing
-
-# Define augmentation parameters
-rotation_angles = [-10, -5, 0, 5, 10]  # rotation angles to randomly choose from
-horizontal_flips = [True, False]  # list of boolean values to randomly choose from
-
-# Define face detection parameters
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-scale_factor = 2  # factor to scale up the bounding box of the face
-
-extraction_rate = 1  # desired frame for extraction
-    
 for video_filename in train_videos:
-    video_path = f"{data_dir}/{video_filename}"
+    start_time = time.time()
+    npz_filename = os.path.join(processed_data_dir, f"{video_filename[:-4]}.npz")
+    if os.path.exists(npz_filename):
+        continue
+    video_path = os.path.join(data_dir, video_filename)
+    cap = cv2.VideoCapture(video_path)
     cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)  # get actual frame rate of video
     num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # get total number of frames in video
@@ -73,6 +84,9 @@ for video_filename in train_videos:
         if not ret:
             break
 
+        # resize cropped face frame to desired dimensions
+        #frame = resized_frames(frame)
+        
         # detect face and scale up bounding box
         face_frame = face_detection(frame)
         if face_frame is None:
@@ -88,10 +102,35 @@ for video_filename in train_videos:
         if len(frames) >= desired_num_frames:
             break
     cap.release()
+
+    if len(frames) == 0:
+        continue
+    
     frames = np.stack(frames, axis=0)
 
     # normalize pixel values to range [0, 1]
     frames = normalize_frames(frames)
+    
+    npz_dirname = os.path.dirname(npz_filename)
+    os.makedirs(npz_dirname, exist_ok=True)
+    np.savez_compressed(npz_filename, frames)
 
-    # save pre-processed frames to disk
-    np.save(f"{data_dir}/{video_filename[:-4]}.npy", frames)
+    end_time = time.time()
+    processing_time = end_time - start_time
+    total_processing_time += processing_time
+    num_processed_videos += 1
+    num_train_videos -= 1
+
+    # calculate average processing time per video
+    avg_processing_time = total_processing_time / num_processed_videos
+    
+    # calculate estimated time remaining
+    time_remaining = avg_processing_time * num_train_videos
+
+    # convert time remaining to hours, minutes, seconds
+    hours, remainder = divmod(time_remaining, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    print(f"Estimated time remaining: {hours:.0f} hours, {minutes:.0f} minutes, {seconds:.0f} seconds")
+    print(f"{num_train_videos} videos remaining")
+    print()
+   
